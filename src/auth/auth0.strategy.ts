e88cb13dc9fb71066b7396e1c6219d2f4c0615a5
Masum Millat <millat@MacBook-Pro-de-Millat.local>
2024-09-25 21:22:47 +0600

    added github workflow action

--- src/auth/auth0.strategy.ts
+++ src/auth/auth0.strategy.ts
@@ -8,9 +8,9 @@ export class Auth0Strategy extends PassportStrategy(Strategy, 'auth0') {
   constructor(private configService: ConfigService) {
     super({
       domain: configService.get<string>('AUTH0_DOMAIN'),
-      clientID: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      callbackURL: process.env.AUTH0_CALLBACK_URL,
+      clientID: configService.get<string>('AUTH0_CLIENT_ID'),
+      clientSecret: configService.get<string>('AUTH0_CLIENT_SECRET'),
+      callbackURL: configService.get<string>('AUTH0_CALLBACK_URL'),
       scope: 'openid profile email',
     });
   }
