17b3f768b7e63a26975e9e531a7b08402b50497b
Masum Millat <masummillat@gmail.com>
2024-09-26 11:09:55 +0600

    update test dependencies

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
+import { Auth0Service } from './auth0.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule, UsersModule, ConfigModule],
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthService, UsersService, Auth0Service],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [JwtModule, ConfigModule],
+      providers: [AuthService, UsersService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
